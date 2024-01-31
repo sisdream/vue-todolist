76d31ee32c4bdbefaaded248cdfef5326a56e950
sisdream <sissipascale2@gmail.com>
2024-01-31 16:45:05 +0100

    feat:array

--- js/script.js
+++ js/script.js
@@ -4,9 +4,37 @@ const app = createApp({
     data(){
         return{
             title: "Vue To Do List",
-        }
+
+            tasks:[
+                {
+                    name: "Task 1",
+                    done: true,
+                },
+                {
+                    name: "Task 2",
+                    done: false,
+                },
+                {
+                    name: "Task 3",
+                    done: true,
+                },
+                {
+                    name: "Task 4",
+                    done: false,
+                },
+                
+            ],
+           
+        };
     },
-    
+    methods:{
+        printHello(){
+            console.log("hello vue");
+        },
+    },
+    mounted(){
+        this.printHello();
+    },
 });
 
 app.mount('#app');
